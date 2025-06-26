399d204c0d9dfbe9915e39b1b37dc2cab706ad29
fridaynight4289@gmail.com <fridaynight4289@gmail.com>
2025-06-26 19:31:56 +0700

    home just started

--- lib/home_view/home.dart
+++ lib/home_view/home.dart
@@ -18,13 +18,100 @@ class _HomeState extends State<Home> {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 18),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             TextField(
               decoration: InputDecoration(
                 hintText: "Search products",
+                prefixIcon: Icon(Icons.search),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(8),
                 ),
+              ),
+            ),
+            SizedBox(height: 18),
+            Row(
+              children: [
+                Text(
+                  "Browse by Skin Types",
+                  style: TextStyle(
+                    color: Color.fromRGBO(32, 76, 62, 1),
+                    fontSize: 20,
+                  ),
+                ),
+                Spacer(),
+                Text(
+                  "View all",
+                  style: TextStyle(
+                    color: Color.fromRGBO(32, 76, 62, 1),
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ActionChip(
+                  label: SizedBox(
+                    width: 50,
+                    child: Center(
+                      child: Text(
+                        "Acne",
+                        style: TextStyle(color: Color.fromRGBO(32, 76, 62, 1)),
+                      ),
+                    ),
+                  ),
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(20),
+                    side: BorderSide(color: Color.fromRGBO(32, 76, 62, 1)),
+                  ),
+                  onPressed: () {},
+                ),
+                ActionChip(
+                  label: SizedBox(
+                    width: 50,
+                    child: Center(
+                      child: Text(
+                        "Dry",
+                        style: TextStyle(color: Color.fromRGBO(32, 76, 62, 1)),
+                      ),
+                    ),
+                  ),
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(20),
+                    side: BorderSide(color: Color.fromRGBO(32, 76, 62, 1)),
+                  ),
+                  onPressed: () {},
+                ),
+                ActionChip(
+                  label: SizedBox(
+                    width: 50,
+                    child: Center(
+                      child: Text(
+                        "Aging",
+                        style: TextStyle(color: Color.fromRGBO(32, 76, 62, 1)),
+                      ),
+                    ),
+                  ),
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(20),
+                    side: BorderSide(color: Color.fromRGBO(32, 76, 62, 1)),
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            Container(
+              child: Stack(
+                children: [
+                  Image.network(
+                    "https://assets.central.co.th//adobe/dynamicmedia/deliver/dm-aid--cfa34aa7-47d3-487d-b0fb-3c8eebb17d29/sk-ii-online-sk2hrftessence160ml-cds12035687-1.jpg?preferwebp=true&quality=60",
+                  ),
+                ],
               ),
             ),
           ],
